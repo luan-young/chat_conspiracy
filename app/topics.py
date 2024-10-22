c6cc9d3e3e720f4d666584a3e57f50842e24dba4
Luan Young <luanpy@gmail.com>
2024-10-21 22:59:27 -0300

    - home add rights texts - fix height issues, kind of - fix page headers

--- app/topics.py
+++ app/topics.py
@@ -1,8 +1,8 @@
 
 topics = [
     {'id': '1', 'title': 'Terra Plana', 'question': 'Você acredita que a terra é plana?'},
-    {'id': '2', 'title': 'Lua Artificial', 'question': 'Você acredita que a lua não é um satélite natural e na verdade é outra coisa?'},
-    {'id': '3', 'title': 'Iluminatis', 'question': 'Você acredita nos Iluminatis?'}
+    {'id': '2', 'title': 'Ida do Homem à Lua', 'question': 'Você acredita que o homem foi à lua?'},
+    {'id': '3', 'title': 'Vacinas', 'question': 'Você acredita que as vacinas fazem mal ao invés de fazer bem?'}
 ]
 
 def find_topic(id):
